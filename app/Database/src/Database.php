35d4e9930b6dcdb7bd6312c47e2ba848e18be2d5
chris.zietlow <aczietlow@gmail.com>
2014-12-08 00:45:40 -0500

    Added skeletal work for database class and tests.

--- app/Database/src/Database.php
+++ app/Database/src/Database.php
@@ -8,8 +8,36 @@
 
 namespace FlightSim\Database;
 
-class Database {
-  public function hello() {
+class Database extends \PDO {
+
+  public function __construct() {
+  }
+
+  /**
+   * Creates PDO object to be used to interact with the database through out the
+   * application.
+   */
+  public function connect() {
+    $info = $this->getConnectionInfo();
+    try {
+      parent::__construct('mysql://' . $info['name'] . ':' . $info['password'] . '@' . $info['uri'] . '/' . $info['database']);
+    } catch (\PDOException $e) {
+      sprintf('Database connection failed: %s', $e->getMessage());
+    }
+
     return TRUE;
+  }
+
+  /**
+   * Get the connection info needed to create the PDO object from the config
+   * file.
+   */
+  public function getConnectionInfo() {
+    return array(
+      'name' => 'root',
+      'password' => 'root',
+      'database' => 'drupal8',
+      'uri' => '127.0.0.1',
+    );
   }
 }
--- app/Database/tests/DatabaseTest.php
+++ app/Database/tests/DatabaseTest.php
@@ -6,8 +6,22 @@
 use FlightSim\Database\Database;
 
 class DatabaseTest extends PHPUnit_Framework_TestCase {
-  public function testDatabaseSaysHello() {
+
+
+
+  public function testDatabaseCredentials() {
     $db = new Database();
-    $this->assertTrue($db->hello());
+    $connection_info = $db->getConnectionInfo();
+    $this->assertNotEmpty($connection_info['database']);
+    $this->assertNotEmpty($connection_info['name']);
+    $this->assertNotEmpty($connection_info['password']);
+    $this->assertNotEmpty($connection_info['uri']);
+
+    return $connection_info;
+  }
+
+  public function testDatabaseConnection() {
+    $db = new Database();
+    $this->assertTrue($db->connect());
   }
 }

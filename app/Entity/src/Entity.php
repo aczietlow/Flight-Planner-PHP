700569bf39c19c718bdfa919af28bd5e9d82645d
chris.zietlow <aczietlow@gmail.com>
2015-01-18 17:31:18 -0500

    Add definition to load the destination objects.

--- app/Entity/src/Entity.php
+++ app/Entity/src/Entity.php
@@ -8,17 +8,34 @@
 
 namespace FlightSim\Entity;
 
+use SebastianBergmann\Exporter\Exception;
+
+/**
+ * Class Entity
+ * @package FlightSim\Entity
+ *
+ * Entity factory for objects needed for calculating the flight plan.
+ */
 class Entity
 {
     /**
      * Gets destination object.
      *
+     * @param
+     *   The type of destination object to be loaded
      * @returns Destination
      *   Returns destination object.
+     *
+     * @TODO Come back and add error handling.
      */
-    public function getDestination()
+    public function getDestination($destinationType)
     {
-
+        // Instantiate the destination object.
+        if (class_exists($destinationType)) {
+            return new $destinationType();
+        } else {
+            throw new Exception("Could not load class $destinationType.");
+        }
     }
 
     public function getVehicle()

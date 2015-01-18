a15620db4f4ae29c829c9e113f83b40252a430bd
chris.zietlow <aczietlow@gmail.com>
2015-01-18 17:57:52 -0500

    Refactored Entity class to reuse method that loads the objects.

--- app/Entity/src/Entity.php
+++ app/Entity/src/Entity.php
@@ -21,29 +21,51 @@ class Entity
     /**
      * Gets destination object.
      *
-     * @param
+     * @param string $destinationType
      *   The type of destination object to be loaded
+     *
      * @returns Destination
      *   Returns destination object.
      */
     public static function getDestination($destinationType)
     {
+        // Instantiate the destination object.
+        $destination = self::load($destinationType);
+
+        // Check that the returned object is a Destination object.
+        if ($destination instanceof Destination) {
+            return $destination;
+        } else {
+            throw new Exception("$destinationType is not a valid destination Object");
+        }
+    }
+
+    public static function getVehicle()
+    {
+
+    }
+
+    /**
+     * Instantiate requested object.
+     *
+     * @param string $type
+     *   The type of object to be instantiated.
+     *
+     * @returns object
+     *   Returns object.
+     */
+    protected function load($type) {
         /* Can not insert a variable into a php namespace.
          * '\FlightSim\Entity\$destinationType' is a syntax error.
          */
         // Define the fully qualified class path as a string.
-        $class = '\\FlightSim\\Entity\\' . $destinationType;
+        $class = '\\FlightSim\\Entity\\' . $type;
 
-        // Instantiate the destination object.
+        // Instantiate the object.
         if (class_exists($class)) {
             return new $class();
         } else {
-            throw new Exception("Could not load class $destinationType.");
+            throw new Exception("Could not load class $type.");
         }
-    }
-
-    public function getVehicle()
-    {
-
     }
 }

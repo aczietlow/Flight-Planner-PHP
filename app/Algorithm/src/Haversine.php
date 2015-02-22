a231a3cfa180fad789e1373f66d80e21167cccfb
Chris Zietlow <zietlow@palantir.net>
2015-02-22 17:09:30 -0500

    Adding skeletal work for haversine calculatoion.

---
+++ app/Algorithm/src/Haversine.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Calculates the shortest distance between two points given latitude and longitude.
+ *
+ * Assumes the earth as a perfect sphere, so not 100% accurate.
+ *
+ * User: aczietlow
+ * Date: 2/22/15
+ */
+
+namespace FlightSim\Algorithm;
+
+
+
+class HaversineAlgorithm
+{
+
+} 
